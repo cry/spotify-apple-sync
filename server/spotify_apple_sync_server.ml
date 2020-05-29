1a3867415a1074aea9b316e6a89434acf15abda1
Carey Li <hello@carey.li>
2020-05-30 02:41:20 +0800

    add basic server command

--- server/spotify_apple_sync_server.ml
+++ server/spotify_apple_sync_server.ml
@@ -1,2 +1,16 @@
 open! Core
 open! Async
+
+let command =
+  let open Command.Let_syntax in
+  Command.async
+    ~summary:"spotify to apple music sync server"
+    (let%map () = return ()
+     and config_file = Command.Param.flag "config-file" (Command.Param.required Command.Param.string) ~doc:"CONFIG get settings from this file"
+     in
+     (fun () ->
+         print_endline config_file |> Deferred.return)
+    )
+;;
+
+let () = Command.run command
